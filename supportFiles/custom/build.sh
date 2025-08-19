d2dad3a8999e92003f3dc69a96151c75812d71ae
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2025-08-19 10:42:00 +0800

    Add files via upload

--- supportFiles/custom/build.sh
+++ supportFiles/custom/build.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 # 基于 https://willhaley.com/blog/custom-debian-live-environment/
 
+set -e
+
+echo "🔧 修复 buster 的源..."
+
+cat > /etc/apt/sources.list <<EOF
+deb http://archive.debian.org/debian buster main contrib non-free
+deb http://archive.debian.org/debian-security buster/updates main
+EOF
+
+echo 'Acquire::Check-Valid-Until "false";' > /etc/apt/apt.conf.d/99no-check-valid-until
+
+apt-get update
+
+echo "🚀 开始执行 build.sh ..."
+
 echo 安装所需工具
 apt-get update
 apt-get -y install debootstrap squashfs-tools xorriso isolinux syslinux-efi  grub-pc-bin grub-efi-amd64-bin mtools dosfstools parted
@@ -9,7 +24,7 @@ echo 创建用于制作镜像的目录
 mkdir -p $HOME/LIVE_BOOT
 
 echo 安装 Debian
-debootstrap --arch=amd64 --variant=minbase buster $HOME/LIVE_BOOT/chroot http://ftp.us.debian.org/debian/
+debootstrap --arch=amd64 --variant=minbase buster $HOME/LIVE_BOOT/chroot http://archive.debian.org/debian/
 
 echo 将支持文件复制到 chroot
 cp -v /supportFiles/installChroot.sh $HOME/LIVE_BOOT/chroot/installChroot.sh
